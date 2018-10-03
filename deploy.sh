d370e8984c0020ee3b68c5e8bf1b0c044a285160
Wallace Silva <aliga12@yahoo.com.br>
2018-10-03 02:32:57 -0300

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 echo "Simple deploy..."
 
+php artisan cache:clear && php artisan config:clear && php artisan clear-compiled
+
 # migrate database if needed
-php artisan migrate --force
+# php artisan migrate --force
