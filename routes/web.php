ad7a9c8488bb431d73623c8aa146d520d0054213
Wallace Silva <aliga12@yahoo.com.br>
2018-10-03 17:09:34 -0300

    endpoints/routes to tourpoints/spots

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ Route::get('/terms', 'HomeController@index')->name('terms');
 # Facebook Login
 Route::get('login/facebook', 'Auth\LoginController@redirectToProvider')->name('fb_login');
 Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback')->name('fb_login_callback');
+
+Route::get('api/tourpoints', 'Api\TourPointController@index')->name('tp_all');
+Route::get('api/tourpoints/me', 'Api\TourPointController@listByUser')->name('tp_all_me');
+Route::get('api/tourpoints/{id}', 'Api\TourPointController@show')->name('tp_show');
+Route::post('api/tourpoints', 'Api\TourPointController@store')->name('tp_store');
+Route::put('api/tourpoints/{id}', 'Api\TourPointController@update')->name('tp_update');
+Route::delete('api/tourpoints/{id}', 'Api\TourPointController@destroy')->name('tp_destroy');
+Route::post('api/tourpoints/{id}/checkin', 'Api\TourPointController@checkIn')->name('tp_checkin');
