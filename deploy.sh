da35c434fd4e4635d8f33685963df2fde2e3a45b
Wallace Silva <aliga12@yahoo.com.br>
2018-10-03 02:22:38 -0300

    removed comments and code not used

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 
 echo "Simple deploy..."
 
-# php -r "file_exists('./database/database.sqlite') || touch('./database/database.sqlite');"
-
 # migrate database if needed
 php artisan migrate --force
