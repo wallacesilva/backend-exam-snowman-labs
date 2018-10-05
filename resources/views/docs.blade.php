b05116617af714c74b538314273b50d8586f21bc
Wallace Silva <aliga12@yahoo.com.br>
2018-10-04 21:36:18 -0300

    fixed title home

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -7,7 +7,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>BackEnd Exam - Docs</title>
+    <title>BackEnd Exam</title>
 </head>
 <body>
     <div class="container text-center mt-5">
